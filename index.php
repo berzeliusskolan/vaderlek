2bc018f01e71b7fa8dffa319e72c6a241f46b1fb
Johan Kellen <johan.kellen@linkoping.se>
2019-03-28 16:29:45 +0000

    Refactor endpoint / with default dates

--- index.php
+++ index.php
@@ -1,6 +1,4 @@
 <?php
-// echo PHP_INT_SIZE === 4 ? '32bit' : '64bit';
-// phpinfo();
 require 'vendor/autoload.php';
 
 use Carbon\Carbon;
@@ -14,13 +12,6 @@ $db = new Database($env);
 require 'Api.php';
 $api = new Api();
 
-// $c = new Carbon();
-// var_dump($c);
-
-// $c1 = Carbon::create(2019,3,8,12,0);
-// var_dump($c1);
-
-
 
 // find out now
 $staticBegin = new Carbon('2019-03-08 12:00:00');
@@ -28,12 +19,6 @@ $staticEnd = new Carbon('2019-03-08 12:00:00');
 $begin = $_GET['begin'] ?? $staticBegin->subDays(7);
 $end = $_GET['end'] ?? $staticEnd;
 
-// $begin = $_GET['begin'] ?? '2019-03-01 00:00:00';
-// $end = $_GET['end'] ?? '2019-03-01 04:00:00';
-// $end = $_GET['end'] ?? Carbon::now();
+// get data
+$api->json($db->getWeather($begin, $end));
 
-// printf("Now: %s", Carbon::now());
-
-// var_dump($db->getWeather($begin, $end));
-$api->json($db->getWeather());
-//echo "hit";
